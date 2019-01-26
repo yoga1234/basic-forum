16654820a7537f429015d98e8e6ec41a9c9a5767
Yogasmara <septianayoga30@gmail.com>
2019-01-26 16:40:00 +0700

    another commit

--- application/views/footer.php
+++ application/views/footer.php
@@ -44,14 +44,13 @@
 <!--script datatable selesai-->
 <!--script pengambilan id mulai-->
 <script type="text/javascript">
-$('#myModal').on('show.bs.modal', function (event) {
-var button = $(event.relatedTarget) // Button that triggered the modal
-var recipient = button.data('whatever') // Extract info from data-* attributes
-// If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-// Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-var modal = $(this)
-modal.find('.delete-link').attr('href', recipient)
-})
+  $('#myModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) //Button yang mentrigger modal
+    var data = button.data('whatever') //Ekstrak data dari data-*
+    //Update modal dilakukan dibawah
+    var modal = $(this)
+    modal.find('.delete-link').attr('href', data)
+  })
 </script>
 <!--script pengambilan id selesai-->
 </html>
